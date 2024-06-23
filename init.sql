a3788949f7de9dcea5cd27925a02b84cb40e2714
Gabriel Henrique <59948825+gab3-dev@users.noreply.github.com>
2024-06-23 19:04:48 +0000

    Updated the tables with the right data types.

--- init.sql
+++ init.sql
@@ -1,44 +1,47 @@
 CREATE TABLE IF NOT EXISTS operadora (
-    id_operadora INT GENERATED ALWAYS AS IDENTITY,
-    data_operacao VARCHAR(100),
+    id_operadora uuid DEFAULT gen_random_uuid(),
+    data_alteracao VARCHAR(11),
     responsavel VARCHAR(100),
     grupo VARCHAR(100),
     codigo_operadora INT UNIQUE,
     operadora VARCHAR(255),
     razao_social VARCHAR(100),
-    cnpj VARCHAR(100) UNIQUE,
+    cnpj VARCHAR(30),
     email VARCHAR(100),
-    telefone VARCHAR(100),
+    telefone VARCHAR(30),
     PRIMARY KEY(id_operadora)
 );
 
 -- INSERT INTO operadora (data_operacao, responsavel, grupo, codigo_operadora, operadora, razao_social, cnpj, email, telefone)
 -- VALUES ('2022-01-01', 'John Doe', 'Group A', 123, 'ABC Telecom', 'ABC Company', '1234567890', 'john.doe@example.com', '123-456-7890');
 
-CREATE TABLE IF NOT EXISTS praca (
-    id_praca INT GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE IF NOT EXISTS pedagio (
+    id_pedagio INT,
     longitude INT,
     latitude INT,
+    nome VARCHAR(50),
     codigo_operadora INT,
-    nome VARCHAR(255),
-    situacao VARCHAR(100),
+    concessionaria VARCHAR(100),
+    situacao VARCHAR(20),
+    sigla VARCHAR(20),
     rodovia VARCHAR(100),
-    km INT,
-    sentido VARCHAR(100),
-    cidade VARCHAR(100),
-    estado VARCHAR(100),
-    codigo_praca INT,
+    km REAL,
+    id_trecho INT,
+    sentido VARCHAR(10),
+    cidade VARCHAR(50),
+    estado VARCHAR(3),
+    codigo VARCHAR(50),
     orientacao VARCHAR(100),
-    tipo VARCHAR(100),
-    jurisdicao VARCHAR(100),
+    tipo VARCHAR(40),
+    jurisdicao VARCHAR(20),
     cobranca_especial BOOLEAN,
-    categoria VARCHAR(100),
-    data_alteracao VARCHAR(100),
+    categoria VARCHAR(3),
+    data_alteracao VARCHAR(11),
     razao_social VARCHAR(100),
-    cnpj VARCHAR(100) UNIQUE,
+    cnpj VARCHAR(30),
     email VARCHAR(100),
-    telefone VARCHAR(100),
-    PRIMARY KEY(id_praca),
+    telefone VARCHAR(30),
+    PRIMARY KEY(id_pedagio),
     CONSTRAINT fk_operadora
       FOREIGN KEY(codigo_operadora) 
         REFERENCES operadora(codigo_operadora)
@@ -56,7 +59,7 @@ CREATE TABLE IF NOT EXISTS praca (
 --     'sentido', sentido,
 --     'cidade', cidade,
 --     'estado', estado,
---     'codigo_praca', codigo_praca,
+--     'codigo_pedagio', codigo_pedagio,
 --     'orientacao', orientacao,
 --     'tipo', tipo,
 --     'jurisdicao', jurisdicao,
@@ -67,4 +70,4 @@ CREATE TABLE IF NOT EXISTS praca (
 --     'cnpj', cnpj,
 --     'email', email,
 --     'telefone', telefone
--- ) FROM praca;
+-- ) FROM pedagio;
