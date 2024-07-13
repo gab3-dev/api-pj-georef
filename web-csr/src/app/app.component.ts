a1ef687deb183ca823862f91a84588bec88c35e0
gab3-dev <gabe.dev@icloud.com>
2024-07-12 23:40:30 -0300

    New - Web page is configured to build with docker-compose.

---
+++ web-csr/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
+import { ColDef } from 'ag-grid-community'; // Column Definition Type Interface
+import { RouterOutlet } from '@angular/router';
+/* Core Data Grid CSS */
+import 'ag-grid-community/styles/ag-grid.css';
+/* Quartz Theme Specific CSS */
+import 'ag-grid-community/styles/ag-theme-quartz.css';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, AgGridAngular],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+
+export class AppComponent {
+  title = 'web-csr';
+
+  // Row Data: The data to be displayed.
+ rowData = [
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ];
+
+  // Column Definitions: Defines the columns to be displayed.
+  colDefs: ColDef[] = [
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" }
+  ];
+}
