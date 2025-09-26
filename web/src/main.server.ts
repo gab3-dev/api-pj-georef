4b55d1d09405cad3e961206ffb7e77802bdd513b
gab3-dev <gabe.dev@icloud.com>
2025-09-26 00:32:18 -0300

    Update node and rust
    Fix node update passing bootstrapContexto to the application

--- web/src/main.server.ts
+++ web/src/main.server.ts
@@ -1,7 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { AppComponent } from './app/default/app.component';
 import { config } from './app/settings/app.config.server';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(AppComponent, config, context);
 
 export default bootstrap;
